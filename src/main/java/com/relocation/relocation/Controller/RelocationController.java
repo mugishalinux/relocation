dbce03e10e46a867bbc8810c7cf5c97efcfa3ee4
MUGISHA PACIFIQUE <67084986+mugishalinux@users.noreply.github.com>
2023-06-06 10:07:21 +0200

    Update RelocationController.java

--- src/main/java/com/relocation/relocation/Controller/RelocationController.java
+++ src/main/java/com/relocation/relocation/Controller/RelocationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Arrays;
 import java.util.Base64;
 
-
+@RestController
 @Controller
 public class RelocationController {
     @Autowired
